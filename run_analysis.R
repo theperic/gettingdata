dfe4d96eb05ef2ee4d6519fbb78d935da67b4db1
unknown <elarsen@SEALT34ECC73.na.blkint.com>
2014-06-17 06:04:04 -0700

    Reads labels
    starting

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,17 @@
 run_analysis <- function(){
-  ## shell
+  ## set up file paths
+  rootdir<-"UCI HAR Dataset/"
+  testdir<-paste(rootdir,"test/", sep="")
+  traindir<-paste(rootdir,"train/", sep="")
+  
+  ## read labels
+  
+  labels_file<-paste(rootdir, "activity_labels.txt", sep="")
+  
+  print(labels_file)
+  
+  labels<-read.table(labels_file)
   
   
+  labels
 }
