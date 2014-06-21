3e16a77cc74546ae02d5fae95d0350094a08c9ee
unknown <elarsen@SEALT34ECC73.na.blkint.com>
2014-06-21 06:46:18 -0700

    Adding readme and starting to parse files for labels
    progress.

--- run_analysis.R
+++ run_analysis.R
@@ -7,11 +7,50 @@ run_analysis <- function(){
   ## read labels
   
   labels_file<-paste(rootdir, "activity_labels.txt", sep="")
-  
-  print(labels_file)
-  
   labels<-read.table(labels_file)
   
   
-  labels
+  print(labels)
+  
+  ## read features
+  
+  features_file<-paste(rootdir, "features.txt", sep="")
+  print(features_file)
+  features<-read.table(features_file)
+  
+  ##creates a vector indicating which columns contain mean values
+  mean_true<-grepl("mean",features[,2])
+  ##print(mean_true)
+  ##print(features[,2])
+  
+  ##creates a vector indicating which columns contain standard dev. values
+  std_true<-grepl("std",features[,2])
+  
+  ##combines the previous vectors to indicate the columns I want for the mean and std values.
+  my_true<-mean_true | std_true
+  print(my_true)
+  
+  ## read measures
+  
+  measures_file<-paste(rootdir, "features_info.txt", sep="")
+  print(measures_file)
+  measures<-read.table(measures_file, skip=12, nrows=17)
+  
+  ##print(measures)
+  
+  ##measures_2<-read.table(measures_file, skip=33, nrows=16)
+  
+  ##print(measures_2)
+  ##
+  
+  ##measures_3<-read.table(measures_file, skip=53, nrows=5)
+  
+##  print(measures_3)
+measures_file<-paste(rootdir, "/test/y_test.txt", sep="")
+print(measures_file)
+measures<-read.table(measures_file)
+print(dim(measures))
+##print(measures)
+
+
 }
